0f664145bd5915294846931ec55d1258ba28a6d7
Marco Bucci <56220142+MACULINX@users.noreply.github.com>
2023-11-29 10:44:36 +0100

    Update 29/11/2023

---
+++ Bucci.Marco.4I.Stampante/Models/Pagina.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucci.Marco._4I.Stampante.Models
+{
+    internal class Pagina
+    {
+        public int B { get ; set; }
+        public int Y { get ; set; }
+        public int M { get; set; }
+        public int C { get; set; }
+
+        public Pagina(int c, int m, int y, int b)
+        {
+            if (c > 3 || m > 3 || y > 3 || b > 3)
+                throw new ArgumentException("Troppo inchiostro");
+
+            C = c;
+            M = m;
+            Y = y;
+            B = b;
+        }
+
+        public Pagina()
+        {
+            var r = new Random();
+
+            C = r.Next(4);
+            M = r.Next(4);
+            Y = r.Next(4);
+            B = r.Next(4);
+        }
+    }
+}
---
+++ Bucci.Marco.4I.Stampante/Models/Stampante.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucci.Marco._4I.Stampante.Models
+{
+    internal class Stampante
+    {
+        public int B { get; set; }
+        public int Y { get; set; }
+        public int M { get; set; }
+        public int C { get; set; }
+        public int Fogli { get; set; }
+
+        public Stampante()
+        {
+            C = M = Y = B = 100;
+
+            Fogli = 200;
+        }
+
+        public void SostituisiciColore(IEnumerable C)
+        {
+
+        }
+
+        public int StatoInchiostro(IEnumerable C)
+        {
+            return ;
+        }
+        public int StatoCarta()
+        {
+            return Fogli;
+        }
+        public void AggiungiCarta(int qta)
+        {
+            Fogli += qta;
+
+            if (Fogli > 200) Fogli = 200;
+        }
+
+        public bool Stampa(Pagina p)
+        {
+            if (Fogli == 0) return false;
+
+            if ( ( p.C >= C) || (p.M >= M) || (p.Y >= Y) || (p.B >= B) ) return false;
+
+            Fogli--;
+
+            B -= p.B;
+            Y -= p.Y;
+            M -= p.M;
+            C -= p.C;
+
+            return true;
+        }
+    }
+}

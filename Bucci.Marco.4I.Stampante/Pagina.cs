1469eb611e1ea5b17fcdf5eebb9010ef52968c9c
Marco Bucci <56220142+MACULINX@users.noreply.github.com>
2023-11-29 09:45:28 +0100

    Aggiungere i file di progetto.

---
+++ Bucci.Marco.4I.Stampante/Pagina.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucci.Marco._4I.Stampante
+{
+    internal class Pagina
+    {
+        int _c;
+        int _m;
+        int _y;
+        int _b;
+
+        public int B { get => _b; }
+        public int Y { get => _y; }
+        public int M { get => _m; }
+        public int C { get => _c; }
+
+        public Pagina(int c, int m, int y, int b)
+        {
+            if (c >3 || m > 3 || y > 3 || b > 3)
+                throw new ArgumentException("Troppo inchiostro");
+
+            _c = c;
+            _m = m;
+            _y = y;
+            _b = b;
+        }
+
+        public Pagina() 
+        { 
+            var r = new Random();
+
+            _c = r.Next(4);
+            _m = r.Next(4);
+            _y = r.Next(4);
+            _b = r.Next(4);
+        }
+    }
+}
---
+++ Bucci.Marco.4I.Stampante/Stampante.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucci.Marco._4I.Stampante
+{
+    internal class Stampante
+    {
+        int _c;
+        int _m;
+        int _y;
+        int _b;
+        int _fogli;
+
+        public Stampante()
+        {
+            _c = 100;
+            _m = 100;
+            _y = 100;
+            _b = 100;
+            _fogli = 200;
+        }
+
+        public void SostituisiciColore(int c)
+        {
+
+        }
+
+        public void AggiungiCarta(int qta)
+        {
+            _fogli += qta;
+
+            if (_fogli > 200) _fogli = 200;
+        }
+
+        public bool Stampa(Pagina p)
+        {
+            if (_fogli == 0) return false;
+
+
+            return true;
+        }
+    }
+}
